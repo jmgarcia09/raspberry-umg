4d7a91d020657c1d8243a65cb216c8a754077b58
Jose Garcia <jose.garcia@bowpi.com>
2018-04-16 22:10:29 -0600

    First

---
+++ src/main/java/com/umg/raspberry/RaspberryApplication.java
@@ -0,0 +1,12 @@
+package com.umg.raspberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/umg/raspberry/web/GpioRestController.java
@@ -0,0 +1,43 @@
+package com.umg.raspberry.web;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Bowpi GT
+ * Created by Jose on 5/04/2018.
+ */
+@RestController
+@RequestMapping("/rasp")
+public class GpioRestController {
+
+    private final GpioController controller;
+
+    @Value("${rasp.valid.pins}")
+    private List<Integer> validPins;
+
+    public GpioRestController() {
+        controller = GpioFactory.getInstance();
+    }
+
+
+    @GetMapping("/{gpioEntry}/toggle")
+    private String toggleGpio(@PathVariable(name = "gpioEntry") int gpioEntry){
+
+        if(validPins.contains(gpioEntry)){
+            GpioPinDigitalOutput digitalPin = controller.provisionDigitalOutputPin(RaspiPin.getPinByAddress(gpioEntry));
+            controller.toggle(digitalPin);
+
+        }else {
+            return "Pin not valid, check configuration";
+        }
+
+        return null;
+    }
+}
