bc7c8479b37c0c7cf67c384e9157bf8c201bc107
jmgarcia09 <Breakmaster09>
2018-04-19 22:30:45 -0600

    Test

--- src/main/java/com/umg/raspberry/web/GpioRestController.java
+++ src/main/java/com/umg/raspberry/web/GpioRestController.java
@@ -5,6 +5,7 @@ import com.pi4j.io.gpio.event.GpioPinAnalogValueChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerAnalog;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.umg.raspberry.MotorPin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Bowpi GT
@@ -34,6 +35,7 @@ public class GpioRestController {
     private String[] pinsConfig;
     private Map<String,Integer> validPins;
     private Map<String,GpioPinDigitalOutput> activePins;
+    private List<MotorPin> motorPins;
 
     public GpioRestController() {
         controller = GpioFactory.getInstance();
@@ -48,8 +50,26 @@ public class GpioRestController {
             logger.info("Init pin {}", pin);
             validPins.put(pin,Integer.parseInt(pin));
         }
-
         logger.info("Loaded pins {}", validPins.toString());
+
+        motorPins = new ArrayList<>();
+        motorPins.add(new MotorPin("31", Arrays.asList(PinState.HIGH,PinState.HIGH,
+                PinState.LOW,PinState.LOW,PinState.LOW,PinState.LOW,PinState.LOW,PinState.HIGH)));
+
+        motorPins.add(new MotorPin("33", Arrays.asList(PinState.LOW,PinState.HIGH,
+                PinState.HIGH,PinState.HIGH,PinState.LOW,PinState.LOW,PinState.LOW,PinState.LOW)));
+
+        motorPins.add(new MotorPin("35", Arrays.asList(PinState.LOW,PinState.LOW,
+                PinState.LOW,PinState.HIGH,PinState.HIGH,PinState.HIGH,PinState.LOW,PinState.LOW)));
+
+        motorPins.add(new MotorPin("37", Arrays.asList(PinState.LOW,PinState.LOW,
+                PinState.LOW,PinState.LOW,PinState.LOW,PinState.HIGH,PinState.HIGH,PinState.HIGH)));
+
+        motorPins
+                .parallelStream()
+                .forEach(
+                motorPin -> activePins.put(motorPin.getPinNumber(),
+                        controller.provisionDigitalOutputPin(RaspiPin.getPinByAddress(Integer.valueOf(motorPin.getPinNumber())))));
     }
 
 
@@ -102,6 +122,35 @@ public class GpioRestController {
         }
 
         return null;
+    }
+
+    @GetMapping("/motor")
+    private String executeMotor() throws InterruptedException {
+
+        ForkJoinPool pool = new ForkJoinPool(4);
+
+        pool.execute(() -> {
+            motorPins.parallelStream().forEach(motorPin -> {
+                GpioPinDigitalOutput pin = activePins.get(motorPin.getPinNumber());
+                for(PinState state : motorPin.getPinStates()){
+                        pin.setState(state);
+                        logger.info("Putting state {} to pin {}", state.getValue(),pin.getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        });
+
+        pool.awaitTermination(9, TimeUnit.SECONDS);
+
+        logger.info("Return all pins to low state for motor.");
+        motorPins.forEach(motorPin -> {
+            activePins.get(motorPin.getPinNumber()).low();
+        });
+        return "Executing motor.";
     }
 
 }
